79ab938efa0e8782fcb2e8c9336a645f8b130084
Kevin Howell <khowell@redhat.com>
2017-09-18 16:18:58 -0400

    Fix issues raised in review, fix es6 linting

--- cockpit/src/subscriptions-client.js
+++ cockpit/src/subscriptions-client.js
@@ -17,17 +17,17 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
-var cockpit = require("cockpit");
-var service = cockpit.dbus('com.redhat.RHSM1', {'superuser': 'require'});
-var registerServer = service.proxy('com.redhat.RHSM1.RegisterServer', '/com/redhat/RHSM1/RegisterServer');
-var attachService = service.proxy('com.redhat.RHSM1.Attach', '/com/redhat/RHSM1/Attach');
-var entitlementService = service.proxy('com.redhat.RHSM1.Entitlement', '/com/redhat/RHSM1/Entitlement');
-var unregisterService = service.proxy('com.redhat.RHSM1.Unregister', '/com/redhat/RHSM1/Unregister');
-var productsService = service.proxy('com.redhat.RHSM1.Products', '/com/redhat/RHSM1/Products');
-var legacyService = cockpit.dbus('com.redhat.SubscriptionManager');  // FIXME replace?
-var _ = cockpit.gettext;
+const cockpit = require("cockpit");
+const service = cockpit.dbus('com.redhat.RHSM1', {'superuser': 'require'});
+const registerServer = service.proxy('com.redhat.RHSM1.RegisterServer', '/com/redhat/RHSM1/RegisterServer');
+const attachService = service.proxy('com.redhat.RHSM1.Attach', '/com/redhat/RHSM1/Attach');
+const entitlementService = service.proxy('com.redhat.RHSM1.Entitlement', '/com/redhat/RHSM1/Entitlement');
+const unregisterService = service.proxy('com.redhat.RHSM1.Unregister', '/com/redhat/RHSM1/Unregister');
+const productsService = service.proxy('com.redhat.RHSM1.Products', '/com/redhat/RHSM1/Products');
+const legacyService = cockpit.dbus('com.redhat.SubscriptionManager');  // FIXME replace?
+const _ = cockpit.gettext;
 
-var client = { };
+const client = { };
 
 cockpit.event_target(client);
 
@@ -41,7 +41,7 @@ client.subscriptionStatus = {
 // we trigger an event called "dataChanged" when the data has changed
 
 function needRender() {
-    var ev = document.createEvent("Event");
+    let ev = document.createEvent("Event");
     ev.initEvent("dataChanged", false, false);
     client.dispatchEvent(ev);
 }
@@ -49,10 +49,10 @@ function needRender() {
 /* we trigger status update via dbus
  * if we don't get a timely reply, consider subscription-manager failure
  */
-var updateTimeout;
+let updateTimeout;
 
 function parseProducts(text) {
-    var products = JSON.parse(text);
+    const products = JSON.parse(text);
     return products.map(function(product) {
         return {
             'productName': product[0],
@@ -65,8 +65,8 @@ function parseProducts(text) {
     });
 }
 
-var gettingDetails = false;
-var getDetailsRequested = false;
+let gettingDetails = false;
+let getDetailsRequested = false;
 function getSubscriptionDetails() {
     if (gettingDetails) {
         getDetailsRequested = true;
@@ -74,25 +74,25 @@ function getSubscriptionDetails() {
     }
     getDetailsRequested = false;
     gettingDetails = true;
-    productsService.ListInstalledProducts('', {})
-        .then(function(result) {
-            client.subscriptionStatus.products = parseProducts(result);
-        })
-        .catch(function(ex) {
-            client.subscriptionStatus.error = ex;
-        })
-        .then(function(output) {
-            gettingDetails = false;
-            if (getDetailsRequested)
-                getSubscriptionDetails();
-            needRender();
-        });
+    productsService.wait(() => {
+        productsService.ListInstalledProducts('', {})
+            .then(result => {
+                client.subscriptionStatus.products = parseProducts(result);
+            })
+            .catch(ex => {
+                client.subscriptionStatus.error = ex;
+            })
+            .then(() => {
+                gettingDetails = false;
+                if (getDetailsRequested)
+                    getSubscriptionDetails();
+                needRender();
+            });
+    });
 }
 
-client.registerSystem = function(subscriptionDetails) {
-    var dfd = cockpit.defer();
-
-    var options = {};
+client.registerSystem = subscriptionDetails => {
+    const options = {};
 
     if (subscriptionDetails.activationKeys) {
         options.activation_keys = {
@@ -108,8 +108,8 @@ client.registerSystem = function(subscriptionDetails) {
             (?::(?=[0-9])([0-9]+))? matches the port if it exists
             (?:(/.+)) matches the rest for the path
         */
-        pattern = new RegExp('^(?:https?://)?([^/:]+)(?::(?=[0-9])([0-9]+))?(?:(/.+))?$');
-        match = pattern.exec(subscriptionDetails.url); // TODO handle failure
+        const pattern = new RegExp('^(?:https?://)?([^/:]+)(?::(?=[0-9])([0-9]+))?(?:(/.+))?$');
+        const match = pattern.exec(subscriptionDetails.url); // TODO handle failure
         options.host = {
             t: 's',
             v: match[1],
@@ -124,7 +124,7 @@ client.registerSystem = function(subscriptionDetails) {
         };
     }
 
-    var connection_options = {};
+    const connection_options = {};
     // proxy is optional
     if (subscriptionDetails.proxy) {
         connection_options.proxy_hostname = {
@@ -134,67 +134,71 @@ client.registerSystem = function(subscriptionDetails) {
         connection_options.proxy_user = {
             t: 's',
             v: subscriptionDetails.proxyUser,
-        }
+        };
         connection_options.proxy_password = {
             t: 's',
             v: subscriptionDetails.proxyPass,
-        }
+        };
     }
 
-    registerServer.Start()
-        .then(function(socket) {
-            console.debug('Opening private bus interface at ' + socket);
-            var private_interface = cockpit.dbus(null, {bus: 'none', address: socket, superuser: 'require'});
-            var registerService = private_interface.proxy('com.redhat.RHSM1.Register', '/com/redhat/RHSM1/Register');
-            if (subscriptionDetails.activationKeys) {
-                return registerService.call('RegisterWithActivationKeys', [subscriptionDetails.org, subscriptionDetails.activationKeys, options, connection_options]);
-            }
-            else {
-                return registerService.call('Register', [subscriptionDetails.org, subscriptionDetails.user, subscriptionDetails.password, options, connection_options]);
-            }
-        })
-        .catch(function(error) {
-            console.error('error initiating registration', error)
-            throw error;
-        })
-        .then(function() {
-            return registerServer.Stop();
-        })
-        .catch(function(error) {
-            console.error('error registering', error);
-            throw error;
-        })
-        .then(function() {
-            return attachService.AutoAttach('', {});
-        })
-        .catch(function(error) {
-            console.error('error stopping registration bus!', error);
-            throw error;
-        })
-        .then(function(result) {
-            console.log('requesting update');
-            requestUpdate();
-        })
-        .catch(function(error) {
-            console.error('error during attach', error);
-            throw error;
-        });
+    registerServer.wait(() => {
+        registerServer.Start()
+            .then(socket => {
+                console.debug('Opening private bus interface at ' + socket);
+                const private_interface = cockpit.dbus(null, {bus: 'none', address: socket, superuser: 'require'});
+                const registerService = private_interface.proxy('com.redhat.RHSM1.Register', '/com/redhat/RHSM1/Register');
+                if (subscriptionDetails.activationKeys) {
+                    return registerService.call('RegisterWithActivationKeys', [subscriptionDetails.org, subscriptionDetails.activationKeys, options, connection_options]);
+                }
+                else {
+                    return registerService.call('Register', [subscriptionDetails.org, subscriptionDetails.user, subscriptionDetails.password, options, connection_options]);
+                }
+            })
+            .catch(error => {
+                console.error('error initiating registration', error);
+                throw error;
+            })
+            .then(() => {
+                return registerServer.Stop();
+            })
+            .catch(error => {
+                console.error('error registering', error);
+                throw error;
+            })
+            .then(() => {
+                return attachService.AutoAttach('', {});
+            })
+            .catch(error => {
+                console.error('error stopping registration bus!', error);
+                throw error;
+            })
+            .then(() => {
+                console.log('requesting update');
+                requestUpdate();
+            })
+            .catch(error => {
+                console.error('error during attach', error);
+                throw error;
+            });
+    });
 };
 
-client.unregisterSystem = function() {
+client.unregisterSystem = () => {
     client.subscriptionStatus.status = "Unregistering";
     needRender();
-    unregisterService.Unregister({})
-        .always(function() {
-            requestUpdate();
-        });
+    unregisterService.wait(() => {
+        unregisterService.Unregister({})
+            .always(() => {
+                requestUpdate();
+            });
+    });
 };
 
 /* request update via DBus
  * possible status values: https://github.com/candlepin/subscription-manager/blob/30c3b52320c3e73ebd7435b4fc8b0b6319985d19/src/rhsm_icon/rhsm_icon.c#L98
  * [ RHSM_VALID, RHSM_EXPIRED, RHSM_WARNING, RHN_CLASSIC, RHSM_PARTIALLY_VALID, RHSM_REGISTRATION_REQUIRED ]
  */
-var subscriptionStatusValues = [
+const subscriptionStatusValues = [
     'RHSM_VALID',
     'RHSM_EXPIRED',
     'RHSM_WARNING',
@@ -203,63 +207,63 @@ var subscriptionStatusValues = [
     'RHSM_REGISTRATION_REQUIRED'
 ];
 function requestUpdate() {
-    legacyService.call(
-        '/EntitlementStatus',
-        'com.redhat.SubscriptionManager.EntitlementStatus',
-        'check_status',
-        [])
-        .always(function() {
-            window.clearTimeout(updateTimeout);
-        })
-        .done(function(result) {
-            client.subscriptionStatus.serviceStatus = subscriptionStatusValues[result[0]];
-            client.getSubscriptionStatus();
-        })
-        .catch(function(ex) {
-            statusUpdateFailed("EntitlementStatus.check_status() failed:", ex);
-        });
+    legacyService.wait(() => {
+        legacyService.call('/EntitlementStatus',
+            'com.redhat.SubscriptionManager.EntitlementStatus',
+            'check_status',
+            []
+        )
+            .always(() => {
+                window.clearTimeout(updateTimeout);
+            })
+            .done(result => {
+                client.subscriptionStatus.serviceStatus = subscriptionStatusValues[result[0]];
+                client.getSubscriptionStatus();
+            })
+            .catch(ex => {
+                statusUpdateFailed("EntitlementStatus.check_status() failed:", ex);
+            });
 
-    /* TODO: Don't use a timeout here. Needs better API */
-    updateTimeout = window.setTimeout(
-        function() {
+        /* TODO: Don't use a timeout here. Needs better API */
+        updateTimeout = window.setTimeout(() => {
             statusUpdateFailed("timeout");
         }, 60000);
+    });
 }
 
-var gettingStatus = false;
-var getStatusRequested = false;
+let gettingStatus = false;
 /* get subscription summary */
-client.getSubscriptionStatus = function() {
+client.getSubscriptionStatus = () => {
     if (gettingStatus) {
-        getStatusRequested = true;
         return;
     }
-    getStatusRequested = false;
     gettingStatus = true;
 
-    entitlementService.GetStatus('')
-        .then(function(result) {
-            var status = JSON.parse(result);
-            client.subscriptionStatus.status = status.status;
-        })
-        .catch(function(error) {
-            client.subscriptionStatus.status = 'Unknown';
-        })
-        .then(function() {
-            gettingStatus = false;
-            getSubscriptionDetails();
-            needRender();
-        });
+    entitlementService.wait(() => {
+        entitlementService.GetStatus('')
+            .then(result => {
+                const status = JSON.parse(result);
+                client.subscriptionStatus.status = status.status;
+            })
+            .catch(() => {
+                client.subscriptionStatus.status = 'Unknown';
+            })
+            .then(() => {
+                gettingStatus = false;
+                getSubscriptionDetails();
+                needRender();
+            });
+    });
 };
 
-client.init = function() {
+client.init = () => {
     /* we want to get notified if subscription status of the system changes */
     legacyService.subscribe(
         { path: '/EntitlementStatus',
           interface: 'com.redhat.SubscriptionManager.EntitlementStatus',
           member: 'entitlement_status_changed'
         },
-        function(path, dbus_interface, signal, args) {
+        () => {
             window.clearTimeout(updateTimeout);
             /*
              * status has changed, now get actual status via command line
@@ -279,7 +283,7 @@ client.init = function() {
           interface: "org.freedesktop.DBUS.Properties",
           member: "PropertiesChanged"
         },
-        function(path, iface, signal, args) {
+        () => {
             client.getSubscriptionStatus();
         }
     );
